e706048cc0cdc19fb2332a8ff7cfede491543bb9
Viwu <sinconsistencia@gmail.com>
2025-11-16 12:16:36 -0600

    split these appart

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import tweepy
-from atproto import Client
+import backend
 
-x_consumer_key = open("x/consumer_key.txt")
-x_consumer_secret = open("x/consumer_secret.txt")
-x_access_token = open("x/access_token.txt")
-x_access_token_secret = open("x/access_token_secret.txt")
-
-x_auth = tweepy.OAuth1UserHandler(
-    x_consumer_key, x_consumer_secret, x_access_token, x_access_token_secret
-)
-
-x_api = tweepy.API(x_auth)
-
-public_tweets = x_api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-
-bluesky_client = Client()
-bluesky_client.login(open("bluesky/identifier.txt"), open("bluesky/password.txt"))
+twitter_api = backend.x_api
+bluesky_client = backend.bluesky_client
